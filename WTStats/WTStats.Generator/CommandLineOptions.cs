2182c5e492562122b89baadf3ec2bf8ca13216a7
Piotr Krupa <piotrkrupa06@gmail.com>
2018-09-02 21:31:19 +0200

    Added CommandLine options

---
+++ WTStats/WTStats.Generator/CommandLineOptions.cs
@@ -0,0 +1,22 @@
+using CommandLine;
+
+namespace WTStats.Generator
+{
+	class CommandLineOptions
+	{
+		[Value(0, MetaName ="Input file", HelpText = "Input file to be processed", Required = true)]
+		public string FilePath { get; set; }
+
+		[Option('o', "output", HelpText = "Output directory", Default = "")]
+		public string OutputDirectory { get; set; }
+
+		[Option("gentxt", HelpText = "Generates txt file")]
+		public bool GenerateTxt { get; set; }
+
+		[Option("genmd", HelpText = "Generates markdown")]
+		public bool GenerateMarkdown { get; set; }
+
+		[Option("genhtml", HelpText = "Generates HTML")]
+		public bool GenerateHTML { get; set; }
+	}
+}
